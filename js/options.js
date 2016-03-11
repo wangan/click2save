7902dddf44d7122241f1e7dbc5b9e72cbad331d8
王安 <anntony.wang@live.com>
2016-03-11 18:03:19 +0800

    go on

--- js/options.js
+++ js/options.js
@@ -5,7 +5,9 @@ window.BB = {};
     if (window.define) {
         return
     }
-    var module = { exports: {} };
+    var module = {
+        exports: {}
+    };
 
     function require(name) {
         return MM[name].call(undefined, require, module, module.exports);
@@ -13,6 +15,7 @@ window.BB = {};
 
     function define(name, func) {
         console.log(name);
+        console.log(module);
         MM[name] = func;
 
         if (name === "Init") {
@@ -88,58 +91,15 @@ define("Event", function(require, module, exports) {
     var tools = require("Tools");
     var chrome = require("Chrome");
     var storage = require("Storage");
+    var actions = require("Action");
 
     exports.Bind = function() {
         document.addEventListener('click', function(e) {
             console.log(e);
-            console.log(e.srcElement.tagName);
-            console.log(e.srcElement.style);
-            console.log(e.srcElement.style.backgroundImage);
-            if (e.altKey === true && e.ctrlKey === true) {
-                var objUrl;
-                switch (e.srcElement.tagName) {
-                    case "IMG":
-                    case "INPUT":
-                        objUrl = e.srcElement.src;
-                        break;
-                    default:
-                        if (e.srcElement.style && e.srcElement.style.backgroundImage) {
-                            if (mRes = e.srcElement.style.backgroundImage.match(/url\(["|']?([^)].*?)["|']?\)/i)) {
-                                if (mRes) {
-                                    objUrl = mRes[1];
-                                }
-                            }
-                        }
-                        break;
-                }
-
-                var index = window.domains ? window.domains[document.domain] : -1;
-                if (!window.rules || !window.domains || !index || (index >= 0 && window.rules[index].status)) {
-
-                    /* Process url */
-                    console.log(objUrl);
-                    if ((index = objUrl.indexOf("?")) > 0)
-                        objUrl = objUrl.substring(0, index);
-
-                    console.log(objUrl);
-                    
-                    console.log(typeof (objUrl));
-                    if ((objUrl && tools.IsPicture(objUrl))) {
-                        chrome.extension.sendRequest(
-                            {
-                                url: objUrl,
-                                baseUrl: window.location,
-                                tagName: e.srcElement.tagName
-                            },
-                            function(response) {
-                                console.log(response);
-                            });
-
-                        e.returnValue = false;
-                        e.stopPropagation();
-                        e.preventDefault();
-                    }
-                }
+            var curAction = e.srcElement.dataset["action"];
+            if (curAction) {
+                console.log(curAction);
+                actions.Invoke(curAction);
             }
         }, true)
     }
@@ -147,16 +107,89 @@ define("Event", function(require, module, exports) {
     return module.exports;
 });
 
+define("Action", function(require, module, exports) {
+    // module.exports = {
+    //     Invoke: function(action) {
+    //         this.Actions[action]();
+    //     },
+    //     Actions: {
+    //         "search-rules": function() {
+    //             console.log("Invoking search-rules");
+    //         }
+    //     }
+    // };
+
+    module.exports.Invoke = function(action) {
+        if (this.Actions && this.Actions[action])
+            this.Actions[action]();
+    };
+    module.exports.Actions = {
+        "search-rules": function() {
+            console.log("Invoking search-rules");
+            var templateStr = " <table><thead><tr><th class='data-id'>Id</th><th class='data-url'>链接地址</th><th class='data-domain'>域名</th><th class='data-location'>存放目录</th><th class='data-status'>状态</th><th class='data-op'>操作</th></tr></thead><tbody>";
+            window.rules.forEach(function(data, index) {
+                templateStr += "<tr><td class='data-id'>" + data.id +
+                    "</td><td class='data-url'>" + data.url +
+                    "</td><td class='data-domain'>" + data.domain +
+                    "</td><td class='data-location'>" + (data.location ? data.location : "--") +
+                    "</td><td class='data-status'><input type='checkbox' disabled checked='" + (data.status ? 'true' : 'false') + "'/>" +
+                    "</td><th class='data-op'><input name='data-op' type='checkbox' value='"+ data.id+ "'/></th></tr>";
+            });
+
+            templateStr += "</tbody></table>";
+            console.log(window.rules);
+            console.log(templateStr);
+
+            document.getElementById("main").innerHTML = templateStr; //.value();
+
+        },
+        "all": function() {
+            console.log("Invoking select all");
+            var all = document.getElementsByName("data-op");
+            console.log(all);
+            for (i = 0; i < all.length; i++) {
+                console.log(all[i].value);
+                if (!all[i].checked) {
+                    all[i].checked = true;
+                } else {
+                    all[i].checked = false;
+                }
+            }
+        }
+    };
+
+    return module.exports;
+});
+
 define("Init", function(require, module, exports) {
+
+    console.log(module);
     module.exports = function(window) {
         var event = require("Event");
+        console.log(event);
         event.Bind();
 
         /* Test data */
         var storage = require("Storage");
-        storage.WriteThis("Rules", [
-            { "domain": "www.cnblogs.com", "url": "http://www.cnblogs.com/", "location": "", "status": false }
-        ]);
+        storage.WriteThis("Rules", [{
+            "id": 1,
+            "domain": "www.cnblogs.com",
+            "url": "http://www.cnblogs.com/",
+            "location": "",
+            "status": false
+        }, {
+            "id": 1,
+            "domain": "www.cnblogs.com",
+            "url": "http://www.cnblogs.com/",
+            "location": "",
+            "status": false
+        }, {
+            "id": 1,
+            "domain": "www.cnblogs.com",
+            "url": "http://www.cnblogs.com/",
+            "location": "",
+            "status": false
+        }]);
 
         storage.ReadThis("Rules", function(data) {
             window.rules = data.Rules;
@@ -169,5 +202,3 @@ define("Init", function(require, module, exports) {
 
     return module.exports;
 });
-
-
